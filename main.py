d2e9485e5295cb900770c6748373bee57dbdb7ea
alireza138812 <alireza09214484373@outlook.com>
2022-01-30 12:53:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 # GUIwinget python source file , by alireza138812
 # importing modules
-from tkinter import *
 import os
+try:
+    from tkinter import *
+finally:
+    import sys
+    print("Tkinter is not installed")
+    sys.exit()
+    
 
 # define funcetions
 
-
 class app():
+
     def installappname():
         installing(enter.get())
         name = enter.get()
         os.system(f"winget install --name {name}")
 
     def installappid():
-        installing(enter.get())
+        app.installing(enter.get())
         id = enter.get()
         os.system(f"winget install --id {id}")
 
+    def installing(appname):
+        text = (f"Installing {appname} , check opened console")
+        Label(window, text=text).pack()
+
+
 
 # window settings
+
 window = Tk()
 window.geometry("250x100")
 window.title("GUIwinget")
@@ -26,19 +38,13 @@ window.title("GUIwinget")
 enter = Entry(window)
 enter.pack()
 
-# Installing Label
-
-
-def installing(appname):
-    text = (f"Installing {appname} , check opened console")
-    Label(window, text=text).pack()
-
-
 # Buttons
+
 Install = Button(window, text="Install",
                  command=app.installappname).place(x=5, y=50)
 
 Installid = Button(window, text="Install with ID",
                    command=app.installappid).place(x=160, y=50)
 # Main loop
+
 window.mainloop()
